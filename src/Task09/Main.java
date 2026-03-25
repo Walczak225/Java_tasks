1de46b9ad6607f3eb29b3ae475d456f2e369b0b9
Walczak225 <mateusz.walczak225@gmail.com>
2026-03-25 12:37:56 +0100

    9 task 1/2 done

---
+++ src/Task09/Main.java
@@ -0,0 +1,14 @@
+package Task09;
+
+public class Main {
+    static void main() {
+        int[][] array = {
+                {1,2,3},
+                {2,3,4}
+        };
+        magicSquare magicSquare = new magicSquare(array);
+        magicSquare.isMagicSquare();
+
+
+    }
+}
---
+++ src/Task09/magicSquare.java
@@ -0,0 +1,20 @@
+package Task09;
+
+public class magicSquare {
+    int[][] array;
+
+    public magicSquare(int[][] array) {
+        this.array = array;
+    }
+
+    public void isMagicSquare() {
+        boolean isTrue = false;
+        int x = array.length;
+        int y = array[1].length;
+       if (x == y)
+    }
+
+
+
+
+}
