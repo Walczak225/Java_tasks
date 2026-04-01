fdeca37c90ad6d2304ce2320078d0a66c4272a14
Walczak225 <mateusz.walczak225@gmail.com>
2026-04-02 01:49:40 +0200

    task09

--- src/Task09/Main.java
+++ src/Task09/Main.java
@@ -3,11 +3,12 @@ package Task09;
 public class Main {
     static void main() {
         int[][] array = {
-                {1,2,3},
-                {2,3,4}
+                {8,1,6},
+                {3,5,7},
+                {4,4,2}
         };
         magicSquare magicSquare = new magicSquare(array);
-        magicSquare.isMagicSquare();
+        System.out.print("Is magic: " + magicSquare.isMagicSquare());
 
 
     }
--- src/Task09/magicSquare.java
+++ src/Task09/magicSquare.java
@@ -1,17 +1,32 @@
 package Task09;
 
 public class magicSquare {
-    int[][] array;
-
+    private int[][] array;
     public magicSquare(int[][] array) {
         this.array = array;
     }
 
-    public void isMagicSquare() {
-        boolean isTrue = false;
-        int x = array.length;
-        int y = array[1].length;
-       if (x == y)
+    public boolean isMagicSquare() {
+        int columnLenght = array.length;
+        int rowLenght = array[0].length;
+        if (columnLenght != rowLenght) return false;
+        int columnSum = 0;
+        int rowSum = 0;
+        int diagonalSumA = 0;
+        int diagonalSumB = 0;
+        for (int i = 0; i < rowLenght; i++) {
+            rowSum += array[0][i];
+            columnSum += array[i][0];
+        }
+        if (columnSum != rowSum) return false;
+
+        for (int i = 0; i < array.length; i++) {
+           diagonalSumA += array[i][i];
+           diagonalSumB += array[i][columnLenght-1-i];
+        }
+        if (diagonalSumA != diagonalSumB) return false;
+
+        return true;
     }
 
 
